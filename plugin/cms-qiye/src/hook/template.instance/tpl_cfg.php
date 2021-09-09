4c8a44b963842511a31500b22f1dfe37fd0c6276
ebcms <56634967+ebcms@users.noreply.github.com>
2021-09-09 22:09:41 +0800

    Delete plugin/cms-qiye directory

--- plugin/cms-qiye/src/hook/template.instance/tpl_cfg.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Ebcms\Template;
-
-/**
- * @var Template $params
- */
-
-$params->addPath('ebcms/cms', __DIR__ . '/../../template', 50);
--- plugin/cms-qiye/src/template/web/common/sidebar.php
+++
@@ -1,2 +0,0 @@
-{if function_exists('tpl_fragment')}{fragment 'plugin.cms-qiye.gonggao', '暂无'}{/if}
-{if function_exists('tpl_fragment')}{fragment 'plugin.cms-qiye.contactus', '暂无'}{/if}
